cc84910cc4d2830f57e883f6f65a398756904a5a
Jonathan Heathcote <mail@jhnet.co.uk>
2014-09-16 11:59:50 +0100

    Try alternative dimension order routes.

--- spinn_time.c
+++ spinn_time.c
@@ -16,12 +16,6 @@
 // Number of cores to use on each chip
 #define CORES_PER_CHIP 1
 
-// Address in SDRAM to store results
-#define RESULT_ADDR(x,y,p) ( ((uint*)SDRAM_BASE_BUF) \
-                             + (((x) + ((y)*WIDTH)) * CORES_PER_CHIP) \
-                             + (p)-1 \
-                           )
-
 // Timer for master sending out requests (us)
 #define MASTER_TIMER_TICK 1000
 
@@ -34,12 +28,47 @@
 #define SOUTH_WEST (1<<4)
 #define SOUTH      (1<<5)
 
+// Get opposite direction for route
+#define OPPOSITE(d) (((d)<<3 | (d)>>3) & 0x3F)
+
+// Alternative dimension ordering for dimension order routing
+#define NUM_DIM_ORDERS 6
+#define DIM_ORDER_XYZ 0
+#define DIM_ORDER_XZY 1
+#define DIM_ORDER_YXZ 2
+#define DIM_ORDER_YZX 3
+#define DIM_ORDER_ZXY 4
+#define DIM_ORDER_ZYX 5
+const uint DIM_ORDERS[6][3] = {
+	{0, 1, 2},
+	{0, 2, 1},
+	{1, 0, 2},
+	{1, 2, 0},
+	{2, 0, 1},
+	{2, 1, 0}
+};
+
+// Direction of each dimension
+const uint DIM_DIRECTIONS[3] = {
+	EAST,
+	NORTH,
+	NORTH_EAST // Note this is technically the opposite the dimension's direction
+};
+
+// Address in SDRAM to store results
+#define RESULT_ADDR(x,y,p,d) ( ((uint*)SDRAM_BASE_BUF) \
+                               + (( (((x) + ((y)*WIDTH)) * CORES_PER_CHIP) \
+                                  + (p)-1) * NUM_DIM_ORDERS) \
+                               + (d) \
+                             )
+
 // Routing key encode/decode
-#define XYZP_TO_KEY(x,y,z,p) ( ((x)&0xFF)<<24 | ((y)&0xFF)<<16 | ((z)&0xFF)<<8 | ((p)&0xFF)<<0 )
+#define XYZPD_TO_KEY(x,y,z,p,d) ( ((x)&0xFF)<<24 | ((y)&0xFF)<<16 | ((z)&0xFF)<<8 | ((p)&0x1F)<<3 | ((d)&0x07) )
 #define KEY_TO_X(k) ( ((k) >> 24) & 0xFF )
 #define KEY_TO_Y(k) ( ((k) >> 16) & 0xFF )
 #define KEY_TO_Z(k) ( ((k) >>  8) & 0xFF )
-#define KEY_TO_P(k) ( ((k) >>  0) & 0xFF )
+#define KEY_TO_P(k) ( ((k) >>  3) & 0x1F )
+#define KEY_TO_D(k) ( ((k) >>  0) & 0x07 )
 
 // X/Y/Z to minimal X/Y/Z conversion
 #define XYZ_TO_MIN_X(x,y,z) ((x) - MEDIAN((x),(y),(z)))
@@ -51,11 +80,12 @@
 #define XY_TO_MIN_Y(x,y) ((y) - MIN((x),(y)))
 #define XY_TO_MIN_Z(x,y) (-MIN((x),(y)))
 
-#define XYP_TO_KEY(x,y,p) (XYZP_TO_KEY( XY_TO_MIN_X((x),(y)) \
-                                      , XY_TO_MIN_Y((x),(y)) \
-                                      , XY_TO_MIN_Z((x),(y)) \
-                                      , p \
-                                      ))
+#define XYPD_TO_KEY(x,y,p,d) (XYZPD_TO_KEY( XY_TO_MIN_X((x),(y)) \
+                                          , XY_TO_MIN_Y((x),(y)) \
+                                          , XY_TO_MIN_Z((x),(y)) \
+                                          , (p) \
+                                          , (d) \
+                                          ))
 
 // The position of this chip in the system
 uint my_x = -1;
@@ -66,6 +96,7 @@ uint my_p = -1;
 uint dest_x = 0;
 uint dest_y = 0;
 uint dest_p = 1;
+uint dest_dim_order = 0;
 
 // Got reply flag
 volatile uint got_reply = FALSE;
@@ -90,6 +121,20 @@ volatile uint recv_time;
 		} \
 	} while (0)
 
+
+// Macros which expand to 0xFF if the given dimension (defined by dim_order) is
+// at that index or lower and zero otherwise. i.e. 0xFF if this dimension has
+// been used by the given index.
+#define DX0F (((DIM_ORDERS[dim_order][0]==0) ? 0xFF : 0x00)       )
+#define DX1F (((DIM_ORDERS[dim_order][1]==0) ? 0xFF : 0x00) | DX0F)
+#define DX2F (((DIM_ORDERS[dim_order][2]==0) ? 0xFF : 0x00) | DX1F)
+#define DY0F (((DIM_ORDERS[dim_order][0]==1) ? 0xFF : 0x00)       )
+#define DY1F (((DIM_ORDERS[dim_order][1]==1) ? 0xFF : 0x00) | DY0F)
+#define DY2F (((DIM_ORDERS[dim_order][2]==1) ? 0xFF : 0x00) | DY1F)
+#define DZ0F (((DIM_ORDERS[dim_order][0]==2) ? 0xFF : 0x00)       )
+#define DZ1F (((DIM_ORDERS[dim_order][1]==2) ? 0xFF : 0x00) | DZ0F)
+#define DZ2F (((DIM_ORDERS[dim_order][2]==2) ? 0xFF : 0x00) | DZ1F)
+
 /**
  * Initialise the routing tables on this chip with a naive 0,0 to any, any to
  * 0,0 routing scheme.
@@ -99,59 +144,69 @@ setup_routing_tables(void)
 {
 	uint entry;
 	
-	uint x = XY_TO_MIN_X(my_x,my_y);
-	uint y = XY_TO_MIN_Y(my_x,my_y);
-	uint z = XY_TO_MIN_Z(my_x,my_y);
+	uint xyz[3] = {
+		XY_TO_MIN_X(my_x,my_y),
+		XY_TO_MIN_Y(my_x,my_y),
+		XY_TO_MIN_Z(my_x,my_y)
+	};
 	
 	// Add entries to accept packets destined for this core
 	for (int p = 1; p <= CORES_PER_CHIP; p++)
-		ADD_RTR_ENTRY( XYZP_TO_KEY(x,y,z,p)
-		             , XYZP_TO_KEY(0xFF,0xFF,0xFF,0xFF)
+		ADD_RTR_ENTRY( XYZPD_TO_KEY(xyz[0],xyz[1],xyz[2],   p,0)
+		             , XYZPD_TO_KEY(  0xFF,  0xFF,  0xFF,0x1F,0)
 		             , CORE(p)
 		             );
 	
-	if (my_x == 0 && my_y == 0) {
-		// Start packets off on the right dimension from the master
-		ADD_RTR_ENTRY( XYZP_TO_KEY(   0,   0,   0,   0)
-		             , XYZP_TO_KEY(0xFF,0xFF,0x00,0x00)
-		             , NORTH_EAST
-		             );
-		ADD_RTR_ENTRY( XYZP_TO_KEY(   0,   0,   0,   0)
-		             , XYZP_TO_KEY(0xFF,0x00,0x00,0x00)
-		             , NORTH
-		             );
-		ADD_RTR_ENTRY( XYZP_TO_KEY(   0,   0,   0,   0)
-		             , XYZP_TO_KEY(0x00,0x00,0x00,0x00)
-		             , EAST
-		             );
-	} else {
-		// Send packets in the right direction to the master
-		if (z)
-			ADD_RTR_ENTRY( XYZP_TO_KEY(   0,   0,   0,   1)
-			             , XYZP_TO_KEY(0xFF,0xFF,0xFF,0xFF)
-			             , SOUTH_WEST
+	// Add routes which allow dimension-order routing with any dimension order
+	for (int dim_order = 0; dim_order < NUM_DIM_ORDERS; dim_order++) {
+		if (my_x == 0 && my_y == 0) {
+			// Start packets off on the right dimension from the master
+			// If the first/second dimensions are zero, route in the third dimension
+			ADD_RTR_ENTRY( XYZPD_TO_KEY(   0,   0,   0,   0, dim_order)
+			             , XYZPD_TO_KEY(DX1F,DY1F,DZ1F,0x00, 0x07)
+			             , DIM_DIRECTIONS[DIM_ORDERS[dim_order][2]]
 			             );
-		else if (y)
-			ADD_RTR_ENTRY( XYZP_TO_KEY(   0,   0,   0,   1)
-			             , XYZP_TO_KEY(0xFF,0xFF,0xFF,0xFF)
-			             , SOUTH
+			// If the first dimension is zero, route in the second dimension
+			ADD_RTR_ENTRY( XYZPD_TO_KEY(   0,   0,   0,   0, dim_order)
+			             , XYZPD_TO_KEY(DX0F,DY0F,DZ0F,0x00, 0x07)
+			             , DIM_DIRECTIONS[DIM_ORDERS[dim_order][1]]
 			             );
-		else if (x)
-			ADD_RTR_ENTRY( XYZP_TO_KEY(   0,   0,   0,   1)
-			             , XYZP_TO_KEY(0xFF,0xFF,0xFF,0xFF)
-			             , WEST
+			// If no dimension is zero, route in the first dimension
+			ADD_RTR_ENTRY( XYZPD_TO_KEY(   0,   0,   0,   0, dim_order)
+			             , XYZPD_TO_KEY(0x00,0x00,0x00,0x00, 0x07)
+			             , DIM_DIRECTIONS[DIM_ORDERS[dim_order][0]]
 			             );
+		} else {
+			// Send packets in the opposite dimension order when returning to the
+			// master.
+			// If the third dimension is not zero, route along it first
+			if (xyz[DIM_ORDERS[dim_order][2]])
+				ADD_RTR_ENTRY( XYZPD_TO_KEY(   0,   0,   0,   1, dim_order)
+				             , XYZPD_TO_KEY(0xFF,0xFF,0xFF,0x1F, 0x07)
+				             , OPPOSITE(DIM_DIRECTIONS[DIM_ORDERS[dim_order][2]])
+				             );
+			else if (xyz[DIM_ORDERS[dim_order][1]])
+				ADD_RTR_ENTRY( XYZPD_TO_KEY(   0,   0,   0,   1, dim_order)
+				             , XYZPD_TO_KEY(0xFF,0xFF,0xFF,0x1F, 0x07)
+				             , OPPOSITE(DIM_DIRECTIONS[DIM_ORDERS[dim_order][1]])
+				             );
+			else if (xyz[DIM_ORDERS[dim_order][0]])
+				ADD_RTR_ENTRY( XYZPD_TO_KEY(   0,   0,   0,   1, dim_order)
+				             , XYZPD_TO_KEY(0xFF,0xFF,0xFF,0x1F, 0x07)
+				             , OPPOSITE(DIM_DIRECTIONS[DIM_ORDERS[dim_order][0]])
+				             );
+		}
+		
+		// Dimension order route packets (x, then y, then z)
+		ADD_RTR_ENTRY( XYZPD_TO_KEY(xyz[0]&DX1F, xyz[1]&DY1F,xyz[2]&DZ1F,   0,dim_order)
+		             , XYZPD_TO_KEY(       DX1F,        DY1F,       DZ1F,0x00,0x07)
+		             , DIM_DIRECTIONS[DIM_ORDERS[dim_order][2]]
+		             );
+		ADD_RTR_ENTRY( XYZPD_TO_KEY(xyz[0]&DX0F, xyz[1]&DY0F,xyz[2]&DZ0F,   0,dim_order)
+		             , XYZPD_TO_KEY(       DX0F,        DY0F,       DZ0F,0x00,0x07)
+		             , DIM_DIRECTIONS[DIM_ORDERS[dim_order][1]]
+		             );
 	}
-	
-	// Dimension order route packets (x, then y, then z)
-	ADD_RTR_ENTRY( XYZP_TO_KEY(   x,   y,   0,   0)
-	             , XYZP_TO_KEY(0xFF,0xFF,0x00,0x00)
-	             , NORTH_EAST
-	             );
-	ADD_RTR_ENTRY( XYZP_TO_KEY(   x,   0,   0,   0)
-	             , XYZP_TO_KEY(0xFF,0x00,0x00,0x00)
-	             , NORTH
-	             );
 }
 
 
@@ -159,8 +214,15 @@ setup_routing_tables(void)
 void
 bounce_mc_packet(uint key, uint _1)
 {
-	spin1_send_mc_packet(XYP_TO_KEY(0,0,1), XYP_TO_KEY(my_x,my_y,my_p), 1);
-	//io_printf(IO_BUF, "Returning bounce from %08x\n", key);
+	spin1_send_mc_packet( XYPD_TO_KEY(0,0,1,KEY_TO_D(key))
+	                    , XYPD_TO_KEY( KEY_TO_X(key)
+	                                 , KEY_TO_Y(key)
+	                                 , KEY_TO_Z(key)
+	                                 , KEY_TO_D(key)
+	                                 )
+	                    , 1
+	                    );
+	io_printf(IO_BUF, "Returning bounce from %08x\n", key);
 }
 
 
@@ -179,29 +241,32 @@ on_tick(uint _1, uint _2)
 {
 	// Store results
 	if (got_reply)
-		*(RESULT_ADDR(dest_x,dest_y,dest_p)) = -(recv_time-send_time);
+		*(RESULT_ADDR(dest_x,dest_y,dest_p,dest_dim_order)) = -(recv_time-send_time);
 	else
-		*(RESULT_ADDR(dest_x,dest_y,dest_p)) = 0;
+		*(RESULT_ADDR(dest_x,dest_y,dest_p,dest_dim_order)) = 0;
 	
 	// Advance through the system
-	if (++dest_p > CORES_PER_CHIP) {
-		dest_p = 1;
-		if (++dest_x >= WIDTH) {
-			dest_x = 0;
-			if (++dest_y >= HEIGHT) {
-				dest_y = 0;
+	if (++dest_dim_order >= NUM_DIM_ORDERS) {
+		dest_dim_order = 0;
+		if (++dest_p > CORES_PER_CHIP) {
+			dest_p = 1;
+			if (++dest_x >= WIDTH) {
+				dest_x = 0;
+				if (++dest_y >= HEIGHT) {
+					dest_y = 0;
+				}
+				io_printf(IO_BUF, "Up to %d %d %d.\n", dest_x, dest_y, dest_p);
 			}
-			io_printf(IO_BUF, "Up to %d %d %d.\n", dest_x, dest_y, dest_p);
 		}
 	}
 	
 	// Stop after sending once to everyone
-	if (dest_x == my_x && dest_y == my_y && dest_p == my_p) {
+	if (dest_x == my_x && dest_y == my_y && dest_p == my_p && dest_dim_order == 0) {
 		spin1_exit(0);
 	}
 	
 	// Send an empty packet to the remote to ping back
-	uint key = XYP_TO_KEY(dest_x,dest_y,dest_p);
+	uint key = XYPD_TO_KEY(dest_x,dest_y,dest_p, dest_dim_order);
 	got_reply = 0;
 	send_time = tc2[TC_COUNT];
 	spin1_send_mc_packet(key, 0, 1);
@@ -238,6 +303,5 @@ c_main() {
 	} else {
 		spin1_callback_on(MCPL_PACKET_RECEIVED, bounce_mc_packet, 1);
 	}
-	
 	spin1_start(TRUE);
 }
--- unpack_results.py
+++ unpack_results.py
@@ -12,26 +12,40 @@ result_file    = sys.argv[1]
 width          = int(sys.argv[2])
 height         = int(sys.argv[3])
 cores_per_chip = int(sys.argv[4])
+num_dim_orders = 6
 
 with open(result_file, "rb") as f:
 	x = 0
 	y = 0
 	p = 0
+	d = 0
+	roundtrips = []
 	while True:
 		data = f.read(4)
 		if not data:
 			break
 		
 		roundtrip = struct.unpack("<L", data)[0]
-		print("%d\t%d\t%d\t%d"%(x,y,p,roundtrip))
+		if roundtrip != 0:
+			roundtrips.append(roundtrip)
 		
-		p += 1
-		if p >= cores_per_chip:
-			p = 0
-			x += 1
-			if x >= width:
-				x = 0
-				print("") # Empty line for gnuplot
-				y += 1
-				if y >= height:
-					y = 0
+		d += 1
+		if d >= num_dim_orders:
+			d = 0
+			p += 1
+			if p >= cores_per_chip:
+				p = 0
+				print("%d\t%d\t%d"%(
+					x,y,
+					( sum(roundtrips)/len(roundtrips)
+					  if roundtrips else 0
+					)
+				))
+				roundtrips = []
+				x += 1
+				if x >= width:
+					x = 0
+					print("") # Empty line for gnuplot
+					y += 1
+					if y >= height:
+						y = 0
